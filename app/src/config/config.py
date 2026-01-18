fe767bdb964b1bf3bc26593a96798255819d5267
Miquel Gómez <miquelgc2003@gmail.com>
2026-01-18 16:32:22 +0100

    Parametrized kokoro

--- app/src/config/config.py
+++ app/src/config/config.py
@@ -11,8 +11,12 @@ class Configuration:
     """Configuration class for the project."""
 
     DATA_PATH: str = os.path.join("..", "data")
-    OUTPUT_PATH: str = os.path.join("..", "logs")
+    OUTPUT_PATH: str = os.path.join("..", "output")
 
+    kokoro_language: str = "es"
+    kokoro_voice: str = "ef_dora"
+    kokoro_rate: int = 24000
+    kokoro_play_speed: float = 1.0
 
     def __post_init__(self):
         ...
