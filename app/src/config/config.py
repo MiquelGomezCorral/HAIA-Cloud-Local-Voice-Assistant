7e7751b1dec0ba54d997ced5ae897cc2df5e79d6
Miquel Gómez <miquelgc2003@gmail.com>
2026-01-18 16:20:33 +0100

    basic RAG, KOKORO WHISPER

--- app/src/config/config.py
+++ app/src/config/config.py
@@ -3,28 +3,16 @@
 Configuration of project variables that we want to have available
 everywhere and considered configuration.
 """
-# import os
-import dataclasses
+import os
 from dataclasses import dataclass
-from argparse import Namespace
 
 @dataclass 
 class Configuration:
     """Configuration class for the project."""
 
-    exp_name: str = "base_name"
-    seed:     int = 42
+    DATA_PATH: str = os.path.join("..", "data")
+    OUTPUT_PATH: str = os.path.join("..", "logs")
 
-    gym_id:          str = None
-    learning_rate: float = 2.5e-4
-    total_timesteps: int = 25_000
-
-    torch_deterministic: bool = True
-    cuda:                bool = True
-
-    track_run:         bool = False
-    wandb_project_name: str = "RL"
-    wandb_entity:       str = None
 
     def __post_init__(self):
         ...
